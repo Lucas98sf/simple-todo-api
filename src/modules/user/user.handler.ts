f4398ff3e2193b979a92f12bee9612109ad271a6
lucas98sf <lucas.98sf@gmail.com>
2022-07-04 14:27:04 -0300

    refactor: variable naming

--- src/modules/user/user.handler.ts
+++ src/modules/user/user.handler.ts
@@ -1,13 +1,13 @@
 import type { Request, Response } from "express";
 
 import type { User } from "./user.model";
-import * as userService from "./user.service";
+import * as service from "./user.service";
 
 export async function createUser(
 	req: Request,
 	res: Response
 ): Promise<Response<User>> {
-	const user = await userService.createUser(req.body as User);
+	const user = await service.createUser(req.body as User);
 
 	return res.json(user);
 }
